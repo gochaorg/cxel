c1c0ea923bcf48ec45c962a9a0e543424397936c
Kamnev Georgiy <nt.gocha@gmail.com>
2020-04-09 23:18:47 +0500

    +add,remove,clone()

--- src/main/java/xyz/cofe/cxel/eval/StaticMethods.java
+++ src/main/java/xyz/cofe/cxel/eval/StaticMethods.java
@@ -89,10 +89,38 @@ public class StaticMethods implements Eterable<Method> {
     }
 
     /**
+     * Поиск методов которые совпадают по типам аргументов
+     * @param paramTypes Типы аргментов
+     * @return методы совпадающие по типу аргументов
+     */
+    public StaticMethods sameArgs( Type ... paramTypes ){
+        if( paramTypes==null )throw new IllegalArgumentException( "paramTypes==null" );
+        return new StaticMethods(filter( m ->{
+            Type[] mParams = m.getParameterTypes();
+            Type[] sParams = paramTypes;
+            if( mParams.length != sParams.length )return false;
+            for( int i=0; i<mParams.length; i++ ){
+                Type mt = mParams[i];
+                Type st = mParams[i];
+                if( !mt.equals(st) )return false;
+            }
+            return true;
+        }));
+    }
+
+    /**
      * Возвращает кол-во методов
      * @return кол-во методов
      */
     public int size(){ return methods.size(); }
+
+    /**
+     * Клонирование
+     * @return клон
+     */
+    public synchronized StaticMethods clone(){
+        return new StaticMethods(this);
+    }
 
     /**
      * Добавляет метод, если метод с такой же сигнатурой определен,
@@ -118,5 +146,25 @@ public class StaticMethods implements Eterable<Method> {
     public synchronized void addAll( Iterable<Method> methods ){
         if( methods==null )throw new IllegalArgumentException( "methods==null" );
         methods.forEach( m->{if(m!=null)add(m);} );
+    }
+
+    /**
+     * Удаляет указанный метод из набора
+     * @param method метод
+     */
+    public synchronized void remove( Method method ){
+        if( method==null )throw new IllegalArgumentException( "method==null" );
+        String key = keyOf(method);
+        methods.remove(method);
+        methodsMap.remove(key);
+    }
+
+    /**
+     * Удаляет указанные методы из набора
+     * @param methods методы
+     */
+    public synchronized void removeAll( Iterable<Method> methods ){
+        if( methods==null )throw new IllegalArgumentException( "methods==null" );
+        methods.forEach( m->{ if(m!=null){ remove(m); } } );
     }
 }
