30cb195fd0ecad0afd5ed03c7971e3a66f791464
Kamnev Georgiy <nt.gocha@gmail.com>
2020-04-17 20:29:59 +0500

    fix operators

--- src/main/java/xyz/cofe/cxel/eval/op/BoolOperations.java
+++ src/main/java/xyz/cofe/cxel/eval/op/BoolOperations.java
@@ -3,12 +3,9 @@ package xyz.cofe.cxel.eval.op;
 import xyz.cofe.cxel.eval.FnName;
 
 public class BoolOperations {
-    @FnName("|") public static boolean or( boolean a, boolean b ){ return a || b; }
-    @FnName("|") public static Boolean or( Boolean a, Boolean b ){ return a || b; }
+    @FnName("||") public static boolean or( boolean a, boolean b ){ return a || b; }
+    @FnName("||") public static Boolean or( Boolean a, Boolean b ){ return a || b; }
 
-    @FnName("&") public static boolean and( boolean a, boolean b ){ return a && b; }
-    @FnName("&") public static Boolean and( Boolean a, Boolean b ){ return a && b; }
-
-//    @FnName("!") public static boolean not( boolean a ){ return !a; }
-//    @FnName("!") public static Boolean not( Boolean a ){ return !a; }
+    @FnName("&&") public static boolean and( boolean a, boolean b ){ return a && b; }
+    @FnName("&&") public static Boolean and( Boolean a, Boolean b ){ return a && b; }
 }
