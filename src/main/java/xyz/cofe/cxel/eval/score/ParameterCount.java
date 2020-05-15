4904360c60eaaf896a9a5fd5f8636a7513a367f3
Kamnev Georgiy <nt.gocha@gmail.com>
2020-05-16 03:32:05 +0500

    javadoc

--- src/main/java/xyz/cofe/cxel/eval/score/ParameterCount.java
+++ src/main/java/xyz/cofe/cxel/eval/score/ParameterCount.java
@@ -1,5 +1,12 @@
 package xyz.cofe.cxel.eval.score;
 
+/**
+ * Возвращает кол-во передаваемых типов данных
+ */
 public interface ParameterCount {
+    /**
+     * Возвращает кол-во передаваемых типов данных
+     * @return кол-во параметров
+     */
     int parameterCount();
 }
